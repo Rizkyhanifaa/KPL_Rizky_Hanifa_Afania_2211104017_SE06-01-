2f79f8872628979750c0be389a48b8cb31e2aafc
Rizky Hanifa Afania <rizkyhanifa33@gmail.com>
2025-03-11 22:09:03 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ 05_Generics/TP/tpmodul5.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class HaloGeneric
+{
+    public static void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric.SapaUser("Hanifaa"); 
+    }
+}
+
