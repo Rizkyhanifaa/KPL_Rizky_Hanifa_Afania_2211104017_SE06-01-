c7623ea0b253aae13b4251fc23eaa652fc5f3722
Rizky Hanifa Afania <rizkyhanifa33@gmail.com>
2025-03-25 16:43:16 +0700

    Menambahkan class DataMahasiswa2211104017

---
+++ 07_Grammar-Based_Input_Processing_Parsing/Jurnal/File program/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //DataMahasiswa2211104017.ReadJSON();
+        //TeamMembers2211104017.ReadJSON();
+        GlossaryItem2211104017.ReadJSON();
+    }
+}
