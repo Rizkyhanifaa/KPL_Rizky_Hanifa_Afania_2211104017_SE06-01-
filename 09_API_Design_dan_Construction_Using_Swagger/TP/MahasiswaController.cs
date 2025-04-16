bbf167fd45fc9374e1e20635e7c859f5549f2841
Rizky Hanifa Afania <rizkyhanifa33@gmail.com>
2025-04-16 19:53:46 +0700

    Menambahkan API mahasiswa

---
+++ 09_API_Design_dan_Construction_Using_Swagger/TP/MahasiswaController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace tp_modul9.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        public class Mahasiswa
+        {
+            public string Nama { get; set; }
+            public string Nim { get; set; }
+        }
+
+        private static List<Mahasiswa> listMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa { Nama = "Rizky Hanifa Afania", Nim = "2211104017" },
+            new Mahasiswa { Nama = "Althafia Defiyandrea L.W", Nim = "2211104011" },
+            new Mahasiswa { Nama = "Muhammad Idham C", Nim = "2211104016" },
+            new Mahasiswa { Nama = "Fauzan Wahyu M", Nim = "2211104027" },
+            new Mahasiswa { Nama = "Andera Singgih P", Nim = "2211104007" },
+        };
+
+        // GET /api/mahasiswa
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return listMahasiswa;
+        }
+
+        // GET /api/mahasiswa/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Mahasiswa> Get(int id)
+        {
+            if (id < 0 || id >= listMahasiswa.Count)
+                return NotFound();
+            return listMahasiswa[id];
+        }
+
+        // POST /api/mahasiswa
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa mhs)
+        {
+            listMahasiswa.Add(mhs);
+        }
+
+        // DELETE /api/mahasiswa/{id}
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (id >= 0 && id < listMahasiswa.Count)
+            {
+                listMahasiswa.RemoveAt(id);
+            }
+        }
+    }
+}
