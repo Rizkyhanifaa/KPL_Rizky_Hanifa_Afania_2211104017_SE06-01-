4d54dc8b9dbed3b2664abbcc1b76013150128661
Rizky Hanifa Afania <rizkyhanifa33@gmail.com>
2025-05-17 19:05:09 +0700

    Menambahkan TP modul 12

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/TP/tpmodul12_2211104017/BilanganHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace tpmodul12_2211104017
+{
+    public static class BilanganHelper
+    {
+        public static string CariTandaBilangan(int a)
+        {
+            if (a < 0) return "Negatif";
+            if (a > 0) return "Positif";
+            return "Nol";
+        }
+    }
+}
---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/TP/tpmodul12_2211104017/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace tpmodul12_2211104017
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void buttonCheck_Click(object sender, EventArgs e)
+        {
+            int input;
+            if (int.TryParse(textBoxInput.Text, out input))
+            {
+                labelOutput.Text = BilanganHelper.CariTandaBilangan(input);
+            }
+            else
+            {
+                labelOutput.Text = "Input tidak valid";
+            }
+        }
+    }
+}
