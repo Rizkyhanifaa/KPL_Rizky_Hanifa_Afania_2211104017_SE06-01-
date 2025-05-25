93c9a3c735fb07a4e84fc711dc0ac0209bd707d6
Rizky Hanifa Afania <rizkyhanifa33@gmail.com>
2025-05-25 10:55:03 +0700

    Menambahkan Jurnal Modul 14

---
+++ 14_Clean_Code/Jurnal/MahasiswaController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace modul14_2211104017.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        // Model Mahasiswa
+        public class Mahasiswa
+        {
+            public string Nama { get; set; }
+            public string Nim { get; set; }
+        }
+
+        // List statis data mahasiswa
+        private static readonly List<Mahasiswa> _mahasiswaList = new()
+        {
+            new Mahasiswa { Nama = "Rizky Hanifa Afania", Nim = "2211104017" },
+            new Mahasiswa { Nama = "Althafia Defiyandrea L.W", Nim = "2211104011" },
+            new Mahasiswa { Nama = "Muhammad Idham C", Nim = "2211104016" },
+            new Mahasiswa { Nama = "Fauzan Wahyu M", Nim = "2211104027" },
+            new Mahasiswa { Nama = "Andera Singgih P", Nim = "2211104007" }
+        };
+
+        // GET: api/mahasiswa
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> GetAllMahasiswa()
+        {
+            return Ok(_mahasiswaList);
+        }
+
+        // GET: api/mahasiswa/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Mahasiswa> GetMahasiswaById(int id)
+        {
+            if (id < 0 || id >= _mahasiswaList.Count)
+            {
+                return NotFound();
+            }
+
+            return Ok(_mahasiswaList[id]);
+        }
+
+        // POST: api/mahasiswa
+        [HttpPost]
+        public ActionResult AddMahasiswa([FromBody] Mahasiswa mahasiswa)
+        {
+            _mahasiswaList.Add(mahasiswa);
+            return CreatedAtAction(nameof(GetMahasiswaById), new { id = _mahasiswaList.Count - 1 }, mahasiswa);
+        }
+
+        // DELETE: api/mahasiswa/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteMahasiswa(int id)
+        {
+            if (id < 0 || id >= _mahasiswaList.Count)
+            {
+                return NotFound();
+            }
+
+            _mahasiswaList.RemoveAt(id);
+            return NoContent();
+        }
+    }
+}
+
+
+
+
